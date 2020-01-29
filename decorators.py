e850396e2c47c72193d093a98437a933a8037380
evgeny <djoongaar@gmail.com>
2020-01-29 14:35:14 +0300

    Log decorator with traceback is done

--- decorators.py
+++ decorators.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+import traceback
 
 if sys.argv[0].find('client') == -1:
     print(sys.argv)
@@ -13,7 +14,11 @@ def log(func):
     """ Функция-декоратор логирования (отправлю логировать сервер) """
     def wrapper(*args, **kwargs):
         res = func(*args, **kwargs)
-        LOG.info(f"Сработала функция: {func.__name__} со следующими аргументами: {args}, {kwargs}")
+        LOG.info(f"Сработала функция: {func.__name__} со следующими аргументами: {args}, {kwargs}. "
+                 f"Вызов из модуля {func.__module__}")
+        stack = traceback.format_stack()
+        for line in range(len(stack)):
+            LOG.info(f"Функция была вызвана из {(stack[line]) if line == len(stack) - 2 else None}")
         return res
     return wrapper
 
@@ -23,6 +28,10 @@ class Log:
     def __call__(self, func):
         def decorator(*args, **kwargs):
             res = func(*args, **kwargs)
-            LOG.info(f"Сработала функция: {func.__name__} со следующими аргументами: {args}, {kwargs}")
+            LOG.info(f"Сработала функция: {func.__name__} со следующими аргументами: {args}, {kwargs}. "
+                     f"Вызов из модуля {func.__module__}")
+            stack = traceback.format_stack()
+            for line in range(len(stack)):
+                LOG.info(f"Функция была вызвана из {(stack[line]) if line == len(stack) - 2 else None}")
             return res
         return decorator

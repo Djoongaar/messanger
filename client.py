9c3b431bf31b029e56a0b31c411c81100be3a48d
evgeny <djoongaar@gmail.com>
2020-01-31 20:06:31 +0300

    modul select() realised

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import sys
 import time
@@ -5,10 +6,45 @@ import socket
 import logging
 from log import client_log_config
 from decorators import Log
+from errors import ReqFieldMissingError, ServerError
 from common.settings import DEFAULT_PORT, ACTION, PRESENCE, TIME, ACCOUNT_NAME, USER, RESPONSE, ERROR, \
-    DEFAULT_IP_ADDRESS
+    DEFAULT_IP_ADDRESS, MESSAGE, MESSAGE_TEXT, SENDER
 from common.utils import get_and_print_message, encode_and_send_message
 LOG = logging.getLogger('client')
+
+
+@Log()
+def message_from_server(message):
+    """Функция - обработчик сообщений других пользователей, поступающих с сервера"""
+    if ACTION in message and message[ACTION] == MESSAGE and \
+            SENDER in message and MESSAGE_TEXT in message:
+        print(f'Получено сообщение от пользователя '
+              f'{message[SENDER]}:\n{message[MESSAGE_TEXT]}')
+        LOG.info(f'Получено сообщение от пользователя '
+                 f'{message[SENDER]}:\n{message[MESSAGE_TEXT]}')
+    else:
+        LOG.error(f'Получено некорректное сообщение с сервера: {message}')
+
+
+@Log()
+def write_message(sock, account_name='Guest'):
+    """Функция запрашивает текст сообщения и возвращает его.
+    Так же завершает работу при вводе подобной комманды
+    """
+    message = input('Введите сообщение для отправки или \'!!!\' для завершения работы: ')
+    if message == '!!!':
+        sock.close()
+        LOG.info('Завершение работы по команде пользователя.')
+        print('Спасибо за использование нашего сервиса!')
+        sys.exit(0)
+    message_dict = {
+        ACTION: MESSAGE,
+        TIME: time.time(),
+        ACCOUNT_NAME: account_name,
+        MESSAGE_TEXT: message
+    }
+    LOG.debug(f'Сформирован словарь сообщения: {message_dict}')
+    return message_dict
 
 
 @Log()
@@ -46,37 +82,83 @@ def process_answer(message):
 
 
 @Log()
+def parse_cmd_args():
+    """ Парсер аргументов командной строки """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('addr', default=DEFAULT_IP_ADDRESS, nargs='?')
+    parser.add_argument('port', default=DEFAULT_PORT, type=int, nargs='?')
+    parser.add_argument('-m', '--mode', default='listen', nargs='?')
+    namespace = parser.parse_args(sys.argv[1:])
+    server_address = namespace.addr
+    server_port = namespace.port
+    client_mode = namespace.mode
+
+    # Проверка номера порта
+    if not 1023 < server_port < 65536:
+        LOG.critical(
+            f'Попытка запуска сервера с указанием неподходящего порта'
+            f'{server_port}. Допустимы адреса с 1024 до 65535.'
+        )
+        sys.exit(1)
+    if client_mode not in ('listen', 'send'):
+        LOG.critical(f'Указан недопустимый режим работы {client_mode}')
+        sys.exit(1)
+
+    return server_address, server_port, client_mode
+
+
+@Log()
 def main():
     """
     Загружаем параметры командной строки
     :return:
     """
-    try:
-        server_address = sys.argv[2]
-        server_port = int(sys.argv[1])
-        if server_port < 1024 or server_port > 65535:
-            LOG.debug("Порт может быть от 1024 до 65535")
-            raise ValueError
-    except IndexError:
-        server_address = DEFAULT_IP_ADDRESS
-        server_port = DEFAULT_PORT
-    except ValueError:
-        LOG.debug("Некорректный номер порта. Должен быть от 1024 до 65535")
-        sys.exit(1)
+    server_address, server_port, client_mode = parse_cmd_args()
+    LOG.info(
+        f'Запущен клиент с параметрами: адрес сервера: {server_address}'
+        f'порт: {server_port}, режим работы: {client_mode}'
+    )
     #  Инициализация сокета и обмен
-    TRANSP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
+        TRANSP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         TRANSP.connect((server_address, server_port))
-    except ConnectionRefusedError:
-        LOG.debug("Подключение не установлено, т.к. сервер отверг запрос на подключение")
-    MSG_TO_SERV = make_presence_message()
-    encode_and_send_message(TRANSP, MSG_TO_SERV)
-    try:
+        encode_and_send_message(TRANSP, make_presence_message())
         answer = process_answer(get_and_print_message(TRANSP))
-        PARAMS = {'answer': answer}
-        LOG.info("answer: %(answer)s", PARAMS)
-    except (ValueError, json.JSONDecodeError):
-        LOG.debug("Не удалось декодировать сообщение сервера")
+        LOG.info(f'Установлено соединение с сервером. Ответ сервера: {answer}')
+    except json.JSONDecodeError:
+        LOG.debug("не удалось декодировать полученнную JSON строку")
+        sys.exit(1)
+    except ServerError as error:
+        LOG.error(f'При установке соединения сервер вернул ошибку: {error.text}')
+        sys.exit(1)
+    except ReqFieldMissingError as missing_error:
+        LOG.error(f'В ответе сервера отсутствует необходимое поле {missing_error.missing_field}')
+        sys.exit(1)
+    except ConnectionRefusedError:
+        LOG.critical(
+            f'Не удалось подключиться к серверу {server_address}:{server_port}, '
+            f'конечный компьютер отверг запрос на подключение.'
+        )
+        sys.exit(1)
+    else:
+        #  Если соединение настроено корректно то начинается обмен сообщений с ними
+        if client_mode == 'send':
+            print('Режим работы - отправка сообщений')
+        else:
+            print('Режим работы - приём сообщений')
+        while True:
+            if client_mode == 'send':
+                try:
+                    encode_and_send_message(TRANSP, write_message(TRANSP))
+                except (ConnectionRefusedError, ConnectionError, ConnectionAbortedError):
+                    LOG.error(f'Соединение с сервером было потеряно')
+                    sys.exit(1)
+            if client_mode == 'listen':
+                try:
+                    message_from_server(get_and_print_message(TRANSP))
+                except (ConnectionRefusedError, ConnectionError, ConnectionAbortedError):
+                    LOG.error(f'Соединение с сервером {server_address} было потеряно')
+                    sys.exit(1)
 
 
 if __name__ == '__main__':
--- launcher.py
+++ launcher.py
@@ -14,9 +14,9 @@ while True:
     elif ACTION == 's':
         PROCESS.append(subprocess.Popen('python server.py', creationflags=subprocess.CREATE_NEW_CONSOLE))
         for i in range(2):
-            PROCESS.append(subprocess.Popen('python client.py -m write', creationflags=subprocess.CREATE_NEW_CONSOLE))
+            PROCESS.append(subprocess.Popen('python client.py -m send', creationflags=subprocess.CREATE_NEW_CONSOLE))
         for i in range(5):
-            PROCESS.append(subprocess.Popen('python client.py -m read', creationflags=subprocess.CREATE_NEW_CONSOLE))
+            PROCESS.append(subprocess.Popen('python client.py -m listen', creationflags=subprocess.CREATE_NEW_CONSOLE))
     elif ACTION == 'x':
         while PROCESS:
             VICTIM = PROCESS.pop()
